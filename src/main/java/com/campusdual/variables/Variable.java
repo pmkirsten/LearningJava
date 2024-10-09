58cd940c5de2178868b7b77846be0bd868f07a3e
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-10-09 13:35:11 +0200

    Terminado ejericio de variables y tipos

---
+++ src/main/java/com/campusdual/variables/Variable.java
@@ -0,0 +1,13 @@
+package com.campusdual.variables;
+
+public class Variable {
+    public static void main(String[] args) {
+        double hours = 2.5;
+        int month = 12;
+        String name = "Ramiro";
+        final int DAY_HOURS = 24;
+        name = "Pablo";
+
+        System.out.println(DAY_HOURS);
+    }
+}
